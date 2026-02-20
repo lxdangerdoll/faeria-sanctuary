03088071889f33fe931a8fc3cd5ede60ec1641ba
Synapse Studios <lxdangerdoll@gmail.com>
2026-02-20 01:00:01 -0500

    updated app.jsx,  package.json, vite.config.js.   Mercy 'Tiny Dancer' Danger <8>

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // The 'base' property is essential for sub-folder deployments like /faeria-sanctuary/
-  base: '/faeria-sanctuary/',
+  base: '/faeria-sanctuary/src/',
   plugins: [react()],
   build: {
     outDir: 'dist',
